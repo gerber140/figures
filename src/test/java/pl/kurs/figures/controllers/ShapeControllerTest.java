0d4ff1108c67b7ed160004f5844b8281727b02ce
Gabriel <gerber@kurs.pl>
2024-03-28 01:11:28 +0100

    controller test update

--- src/test/java/pl/kurs/figures/controllers/ShapeControllerTest.java
+++ src/test/java/pl/kurs/figures/controllers/ShapeControllerTest.java
@@ -1,54 +1,35 @@
 package pl.kurs.figures.controllers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
-import org.springframework.data.domain.AuditorAware;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.security.test.context.support.WithMockUser;
-import pl.kurs.figures.TestAuditingConfiguration;
 import pl.kurs.figures.command.CreateShapeCommand;
 import pl.kurs.figures.command.Type;
-import pl.kurs.figures.criteria.ShapeSearchCriteria;
-import pl.kurs.figures.dto.ShapeDTO;
-import pl.kurs.figures.dto.SquareDTO;
-import pl.kurs.figures.model.Circle;
-import pl.kurs.figures.model.Rectangle;
-import pl.kurs.figures.model.Shape;
-import pl.kurs.figures.model.Square;
-import pl.kurs.figures.repository.ShapeRepository;
 import pl.kurs.figures.security.entity.Role;
 import pl.kurs.figures.security.entity.User;
 import pl.kurs.figures.security.repository.UserRepository;
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 
 import static org.hamcrest.Matchers.hasSize;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -67,24 +48,50 @@ class ShapeControllerTest {
     private ObjectMapper objectMapper;
     private User user;
 
+//    @BeforeEach
+//    public void setup() {
+//        user = setupUser();
+//        this.postman = MockMvcBuilders
+//                .webAppContextSetup(this.webApplicationContext)
+//                .build();
+//    }
+//
+//    public User setupUser() {
+//        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+//        Role userRole = Role.USER;
+//        User testUser = new User("user", bCryptPasswordEncoder.encode("user"), userRole);
+//        return userRepository.save(testUser);
+//    }
+
 
     @BeforeEach
     public void setup() {
-        user = setupUser();
+        user = setupUser("user", Role.USER);
         this.postman = MockMvcBuilders
                 .webAppContextSetup(this.webApplicationContext)
                 .build();
     }
 
-    public User setupUser() {
+    public User setupUser(String username, Role role) {
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-        Role userRole = Role.USER;
-        User testUser = new User("user", bCryptPasswordEncoder.encode("user"), userRole);
+        User testUser = new User(username, bCryptPasswordEncoder.encode("user"), role);
         return userRepository.save(testUser);
     }
 
+    private void authenticateAsAdmin() {
+        List<SimpleGrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("ADMIN"));
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken("adminUser", null, authorities);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private void authenticateAsTestUser(String username) {
+        List<SimpleGrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("USER"));
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(username, null, authorities);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
     @Test
-    @WithMockUser(username = "testUser", authorities = {"USER"})
+    @WithMockUser(username = "user", authorities = {"USER"})
     void shouldReturnExceptionWhenInvalidShapeParameters() throws Exception {
         CreateShapeCommand command = new CreateShapeCommand(Type.SQUARE, List.of(5.0, 7.0));
 
@@ -92,7 +99,7 @@ class ShapeControllerTest {
     }
 
     @Test
-    @WithMockUser(username = "testUser", authorities = {"USER"})
+    @WithMockUser(username = "user", authorities = {"USER"})
     void shouldCreateShapeAndReturnShapeDto() throws Exception {
         // given
         CreateShapeCommand command = new CreateShapeCommand(Type.SQUARE, List.of(5.0));
@@ -103,27 +110,71 @@ class ShapeControllerTest {
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.id").exists())
                 .andExpect(jsonPath("$.type").value("SQUARE"))
-                .andExpect(jsonPath("$.createdBy").value("testUser"))
+                .andExpect(jsonPath("$.createdBy").value("user"))
                 .andExpect(jsonPath("$.createdAt").value(expectedDateTime));
     }
 
-
     @Test
-    @WithMockUser(username = "testUser", authorities = {"USER"})
+    @WithMockUser(username = "user", authorities = {"USER"})
     void shouldSearchShapesByCriteria() throws Exception {
         addShape(new CreateShapeCommand(Type.SQUARE, List.of(5.0)));
         addShape(new CreateShapeCommand(Type.SQUARE, List.of(15.0)));
+        addShape(new CreateShapeCommand(Type.SQUARE, List.of(25.0)));
         addShape(new CreateShapeCommand(Type.RECTANGLE, List.of(5.0, 7.0)));
         addShape(new CreateShapeCommand(Type.CIRCLE, List.of(5.0)));
 
 
         postman.perform(get("/api/v1/shapes")
                 .param("type", "SQUARE")
-                .param("sideFrom", "10"))
+                .param("sideFrom", "10")
+                .param("sideTo", "20"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(1)))
                 .andExpect(jsonPath("$[0].type").value("SQUARE"))
                 .andExpect(jsonPath("$[0].side").value(15.0));
+
+    }
+
+    @Test
+    @WithMockUser(username = "user", authorities = {"USER"})
+    void shouldSearchShapesAsUser() throws Exception {
+        addShape(new CreateShapeCommand(Type.SQUARE, List.of(5.0)));
+        addShape(new CreateShapeCommand(Type.RECTANGLE, List.of(5.0, 7.0)));
+        addShape(new CreateShapeCommand(Type.CIRCLE, List.of(5.0)));
+
+        authenticateAsTestUser("otherUser");
+
+        addShape(new CreateShapeCommand(Type.SQUARE, List.of(10.0)));
+        addShape(new CreateShapeCommand(Type.SQUARE, List.of(12.0)));
+
+        postman.perform(get("/api/v1/shapes"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].type").value("SQUARE"))
+                .andExpect(jsonPath("$[0].side").value(10.0))
+                .andExpect(jsonPath("$[1].type").value("SQUARE"))
+                .andExpect(jsonPath("$[1].side").value(12.0));
+    }
+
+    @Test
+    @WithMockUser(username = "user", authorities = {"USER"})
+    void shouldSearchShapesAsAdmin() throws Exception {
+        addShape(new CreateShapeCommand(Type.SQUARE, List.of(5.0)));
+        addShape(new CreateShapeCommand(Type.RECTANGLE, List.of(5.0, 7.0)));
+        addShape(new CreateShapeCommand(Type.CIRCLE, List.of(5.0)));
+
+        String username ="otherUser";
+        authenticateAsTestUser(username);
+
+        addShape(new CreateShapeCommand(Type.SQUARE, List.of(15.0)));
+        addShape(new CreateShapeCommand(Type.RECTANGLE, List.of(5.0, 7.0)));
+        addShape(new CreateShapeCommand(Type.CIRCLE, List.of(5.0)));
+
+        authenticateAsAdmin();
+
+        postman.perform(get("/api/v1/shapes"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(6)));
 
     }
 
